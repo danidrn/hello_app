34f70df401849df595065ae9177419b2e29c0cb1
danidrn <daniela.danirn@gmail.com>
2018-11-06 03:53:13 +0000

    Volta a msg para Olar

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root 'controller_name#action_name'
   #root 'application#hello'
-  root 'application#goodbye'
-  #root 'application#olar'
+  #root 'application#goodbye'
+  root 'application#olar'
 end
